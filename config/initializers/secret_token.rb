3ccba950bb93dde1c61a5df27ad8a6c75287f37c
Mel Sherrin <msherrin@gmail.com>
2014-04-26 20:53:00 +1000

    Created welcome controller with index and about views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = 'b798e44821fc28869309ce2ce3f0671f1dd314e7a7e446d4a3b96d71f2af6dafd25229e82eb2fb7c5b709beb24c047cc7a152df01a796193d776fad85a0031f6'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Bloccit::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+  root to: 'welcome#index'
+end
