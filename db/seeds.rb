84c7222936188984444d8a69a4e92a5db2072661
Mel Sherrin <msherrin@gmail.com>
2014-05-02 14:47:37 +1000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
+
+
 # Create Posts
 50.times do
   Post.create(
@@ -16,6 +38,13 @@ posts = Post.all
     body: Faker::Lorem.paragraph
   )
 end
+
+User.first.update_attributes(
+  email: msherrin@gmail.com,
+  password: Xfiles01,
+)
+
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
